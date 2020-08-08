1d71d824c34873e36de9bad4bfaf34b33bc7b4d9
Can <canhtart@gmail.com>
2020-08-08 20:20:49 +0200

    doc(compute.angle): Added docstring to functions that were lacking. Moved to NumPy docstring format
    refactor(compute.angle): Naming scheme of variables were changed to improve the reflection of their content.
    
    Changed their use accordingly

--- bikipy/compute/angle.py
+++ bikipy/compute/angle.py
@@ -4,31 +4,67 @@ from typing import Union
 import numpy as np
 
 
-POINT_NAME_TO_INDEX = {"point_a": 0, "point_b": 1, "point_c": 2}
+POINT_NAME_TO_INDEX = {
+    "row_vectors_point_a": 0,
+    "row_vectors_point_b": 1,
+    "row_vectors_point_c": 2,
+}
 
 
-def _unit_vector(vector):
-    """ Returns the unit vector of the vector. """
-    return vector / np.linalg.norm(vector)
+def _unit_vector(row_vectors) -> np.ndarray:
+    """ Computes unit vector, i.e. vector/<norm of the vector>
 
+    Parameters
+    ----------
+    row_vectors: np.ndarray-like
+        Array of row vector(s)
 
-def _find_meridian_point(region_of_interest_data):
-    return np.array([np.median(component) for component in region_of_interest_data.T])
-
-
-def inner_clockwise_angel_2d(vector_a, vector_b) -> np.array:
-    """ Computes the inner_angle in radians between two vectors
-
-        :param vector_a: numpy array containing the location of per time
-        :param vector_b: vector pair of vector_a
-
-        >>> inner_clockwise_angel_2d((1, 0, 0), (0, 1, 0))
-        1.5707963267948966      # pi / 2
-        >>> inner_clockwise_angel_2d((1, 0, 0), (1, 0, 0))
-        0.0
-        >>> inner_clockwise_angel_2d((1, 0, 0), (-1, 0, 0))
-        3.141592653589793       # pi
+    Returns
+    -------
+    np.ndarray
     """
+    return row_vectors / np.linalg.norm(row_vectors)
+
+
+def _find_median_vector(row_vectors: np.ndarray) -> np.ndarray:
+    """ Computes the median point from a row vectors
+
+    Median of each component -> combine medians to create median point. Note that this point doesn't exist
+
+    Parameters
+    ----------
+    row_vectors: np.ndarray
+        Array of row vectors
+
+    Returns
+    -------
+    np.ndarray
+    """
+    return np.array([np.median(component) for component in row_vectors.T])
+
+
+def inner_clockwise_angel_2d(vector_a, vector_b) -> np.ndarray:
+    """ Computes the clockwise inner_angle in radians between two vectors
+    
+    Parameters
+    ----------
+    vector_a: np.ndarray
+        Array of row vectors
+    vector_b: np.ndarray
+        Array of row vectors
+
+    Returns
+    -------
+    np.ndarray
+    
+    >>> inner_clockwise_angel_2d((1, 0, 0), (0, 1, 0))
+    1.5707963267948966      # pi / 2
+    >>> inner_clockwise_angel_2d((1, 0, 0), (1, 0, 0))
+    0.0
+    >>> inner_clockwise_angel_2d((1, 0, 0), (-1, 0, 0))
+    3.141592653589793       # pi
+    """
+
     vector_1_u = np.apply_along_axis(_unit_vector, 1, np.asanyarray(vector_a))
     vector_2_u = np.apply_along_axis(_unit_vector, 1, np.asanyarray(vector_b))
 
@@ -46,14 +82,36 @@ def inner_clockwise_angel_2d(vector_a, vector_b) -> np.array:
     return np.pi - np.arctan2(determinants, dot_prod)
 
 
-def angle_over_time(
-    point_a: np.array,
-    point_b: np.array,
-    point_c: np.array,
+def compute_angles_from_vectors(
+    row_vectors_point_a: np.ndarray,
+    row_vectors_point_b: np.ndarray,
+    row_vectors_point_c: np.ndarray,
     median_points: Union[str, list, None] = None,
     degrees: bool = False,
 ):
-    points = [point_a, point_b, point_c]
+    """ Compute the angle between three groups of vectors
+
+    Computed with respect to their index in their respective store.
+
+    Parameters
+    ----------
+    row_vectors_point_a: np.ndarray
+        Array of row vectors
+    row_vectors_point_b: np.ndarray
+        Array of row vectors that is the joint between the two other groups of vectors
+    row_vectors_point_c: np.ndarray
+        Array of row vectors
+    median_points: str, list; optional
+        Anchor one or several row vectors to their respective median. Information about median computation in _find_median_vector()
+    degrees: bool; default False
+        If True, convert resulting angle data to degrees
+
+    Returns
+    -------
+    np.ndarray
+    """
+
+    points = [row_vectors_point_a, row_vectors_point_b, row_vectors_point_c]
     if any(point.dtype == "object" for point in points):
         warn("At least one of the arrays consists solely of NaN (Not a Number) objects")
         return np.full((points[0].size,), np.nan)
@@ -61,10 +119,10 @@ def angle_over_time(
     if isinstance(median_points, list):
         for label in median_points:
             index = POINT_NAME_TO_INDEX[label]
-            points[index] = _find_meridian_point(points[index])
+            points[index] = _find_median_vector(points[index])
     elif isinstance(median_points, str):
         index = POINT_NAME_TO_INDEX[median_points]
-        points[index] = _find_meridian_point(points[index])
+        points[index] = _find_median_vector(points[index])
     else:
         msg = "median_points has to be list, string or None"
         ValueError(msg)
@@ -74,9 +132,35 @@ def angle_over_time(
     return np.rad2deg(computation) if degrees else computation
 
 
-def dlc_angle_over_time(
-    df: DataFrame, point_a_name: str, point_b_name: str, point_c_name: str, *args, **kwargs,
+def dlc_compute_angles_from_vectors(
+    df: DataFrame,
+    point_a_name: str,
+    point_b_name: str,
+    point_c_name: str,
+    *args,
+    **kwargs,
 ):
+    """ compute_angles_from_vectors wrapper for DataFrames generated from DeepLabCut 2d result files
+
+    Parameters
+    ----------
+    df: DataFrame
+        Data from DeepLabCut ingested as a pd.DataFrame
+    point_a_name: str
+        Name of vector group
+    point_b_name: str
+        Name of the vector that is the joint between the two other groups
+    point_c_name: str
+        Name of vector group
+    args
+        Arguments for compute_angles_from_vectors
+    kwargs
+        Keyword arguments for compute_angles_from_vectors
+
+    Returns
+    -------
+    dict: {Angle, Likelihood}
+    """
     from bikipy.utils.deeplabcut import (
         reduce_likelihoods,
         get_region_of_interest_data,
@@ -90,6 +174,6 @@ def dlc_angle_over_time(
     ]
 
     return {
-        "Angle": angle_over_time(*point_set, *args, **kwargs),
+        "Angle": compute_angles_from_vectors(*point_set, *args, **kwargs),
         "Likelihood": likelihood,
     }
--- tests/compute/test_angle.py
+++ tests/compute/test_angle.py
@@ -1,13 +1,16 @@
 from pathlib import Path
 import numpy as np
 
-from bikipy.compute.angle import dlc_angle_over_time, inner_clockwise_angel_2d
+from bikipy.compute.angle import (
+    dlc_compute_angles_from_vectors,
+    inner_clockwise_angel_2d,
+)
 from bikipy import DeepLabCutReader
 
 HDF_PATH = Path(__file__).parent.parent.resolve() / "example_data/data_for_angle.h5"
 
 
-def test_angle_over_time():
+def test_compute_angles_from_vectors():
     test_data = DeepLabCutReader.from_hdf(
         HDF_PATH,
         video_res=(1280, 720),
@@ -16,11 +19,14 @@ def test_angle_over_time():
         min_likelihood=0.95,
     )
 
-    tail_base_tip = dlc_angle_over_time(
-        test_data.df, point_a_name="tail_base", point_b_name="tail_mid", point_c_name="tail_tip"
+    tail_base_tip = dlc_compute_angles_from_vectors(
+        test_data.df,
+        point_a_name="tail_base",
+        point_b_name="tail_mid",
+        point_c_name="tail_tip",
     )
 
-    head_tail = dlc_angle_over_time(
+    head_tail = dlc_compute_angles_from_vectors(
         test_data.df,
         point_a_name="mid-left_ear-right_ear",
         point_b_name="tail_base",
@@ -65,6 +71,3 @@ def test_clockwise_2d():
     three_point_vector_path(((0, 0),), ((1, 0),), ((1, 1),), np.pi / 2)
 
     three_point_vector_path(((0, 0),), ((1, 0),), ((1, -1),), 3 * np.pi / 2)
-
-
-test_angle_over_time()
