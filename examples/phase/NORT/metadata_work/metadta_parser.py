685e1dade7166f568c70cc50ed1907305185bd9f
Can Hicabi Tartanoglu <canhtart@gmail.com>
2022-07-07 20:03:47 +0200

    update

--- examples/phase/NORT/metadata_work/metadta_parser.py
+++ examples/phase/NORT/metadata_work/metadta_parser.py
@@ -4,9 +4,18 @@ perimeter_mapper = {"1": 1, "4": 1, "2": 2, "3": 2}
 
 df = pd.read_excel("metadata_raw.ods", index_col=[0, 1])
 df.loc[:, "TrialNumber"] = df.loc[:, "Video_file_name"].map(lambda x: x.strip().split(".")[0].split(" ")[1]).astype(int)
-df.loc[:, "Perimeter"] = df.loc[:, "Apparatus"].map(lambda x: perimeter_mapper[x.strip().split("-")[-1]])
+
+# Perimeter assignment
+df.loc[:, "Apparatus"] = df.loc[:, "Apparatus"].map(lambda x: x.strip().split("-")[1]).astype(int)
+
+
+df.loc[pd.IndexSlice["A", 1], "Perimeter"] = df.loc[pd.IndexSlice["A", 1], ["Apparatus", "Test"]].map(
+    lambda x: f"training_{x}"
+)
+df.loc[pd.IndexSlice["A", 2], "Perimeter"] = df.loc[pd.IndexSlice["A", 1], ["Apparatus", "Test"]].map(
+    lambda x: f"novel_{x}"
+)
 
 df.set_index("TrialNumber", append=True, inplace=True)
 df.sort_index(inplace=True)
-
 df.to_excel("metadata.xlsx", sheet_name="phase")
