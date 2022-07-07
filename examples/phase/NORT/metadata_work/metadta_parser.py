fc829c82f540dc098c8b244de44746f0689f0b95
Can Hicabi Tartanoglu <canhtart@gmail.com>
2022-07-07 20:40:00 +0200

    update

--- examples/phase/NORT/metadata_work/metadta_parser.py
+++ examples/phase/NORT/metadata_work/metadta_parser.py
@@ -1,3 +1,4 @@
+import numpy as np
 import pandas as pd
 
 perimeter_mapper = {"1": 1, "4": 1, "2": 2, "3": 2}
@@ -6,14 +7,19 @@ df = pd.read_excel("metadata_raw.ods", index_col=[0, 1])
 df.loc[:, "TrialNumber"] = df.loc[:, "Video_file_name"].map(lambda x: x.strip().split(".")[0].split(" ")[1]).astype(int)
 
 # Perimeter assignment
-df.loc[:, "Apparatus"] = df.loc[:, "Apparatus"].map(lambda x: x.strip().split("-")[1]).astype(int)
+df.loc[:, "Apparatus"] = df.loc[:, "Apparatus"].map(lambda x: x.strip().split("-")[2]).astype(int)
 
 
-df.loc[pd.IndexSlice["A", 1], "Perimeter"] = df.loc[pd.IndexSlice["A", 1], ["Apparatus", "Test"]].map(
-    lambda x: f"training_{x}"
-)
-df.loc[pd.IndexSlice["A", 2], "Perimeter"] = df.loc[pd.IndexSlice["A", 1], ["Apparatus", "Test"]].map(
-    lambda x: f"novel_{x}"
+def a1_mapper(ser):
+    test = ser["Test"]
+    if test == 0:
+        return np.nan
+    return f"{test_mapper[test]}_{ser['Apparatus']}"
+
+
+test_mapper = {1: "training", 2: "novel"}
+df.loc[pd.IndexSlice["A", 1], "Perimeter"] = df.loc[pd.IndexSlice["A", 1], ["Apparatus", "Test"]].apply(
+    a1_mapper, axis=1
 )
 
 df.set_index("TrialNumber", append=True, inplace=True)
